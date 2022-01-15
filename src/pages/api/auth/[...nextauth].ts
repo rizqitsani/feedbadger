709f73611703fc0b83cfe3bfcebb95b5b6c9834e
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-15 11:33:43 +0700

    feat: add magic link auth for testing

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
 import GitHubProvider from 'next-auth/providers/github';
 
 import { prisma } from '@/lib/prisma';
@@ -10,6 +11,11 @@ export default NextAuth({
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
     }),
   ],
   debug: process.env.NODE_ENV === 'development',
